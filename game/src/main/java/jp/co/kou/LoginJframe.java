3e054855adb1770552166b797b11353f6efba7ac
koutyousinn <gzchly1987@gmail.com>
2025-06-21 11:15:15 +0900

    初めてのcommit

---
+++ game/src/main/java/jp/co/kou/LoginJframe.java
@@ -0,0 +1,16 @@
+package main.java.jp.co.kou;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class LoginJframe extends JFrame{
+	
+	public LoginJframe() {
+		this.setSize(488, 430);
+		this.setAlwaysOnTop(true);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+}
---
+++ game/src/main/java/jp/co/kou/RegisterJframe.java
@@ -0,0 +1,15 @@
+package main.java.jp.co.kou;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class RegisterJframe extends JFrame{
+	
+	public RegisterJframe () {
+		this.setSize(488, 500);
+		this.setAlwaysOnTop(true);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
